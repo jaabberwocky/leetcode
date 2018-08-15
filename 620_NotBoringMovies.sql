c7c0edd40f0a58c46279a3677a09211dc39b7033
tobias <tobiasleongzhunmun@gmail.com>
2018-08-15 22:21:06 +0800

    add 620sql

---
+++ 620_NotBoringMovies.sql
@@ -0,0 +1,9 @@
+#URL: https://leetcode.com/problems/not-boring-movies/description/
+#Desc: X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
+#Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
+
+SELECT *
+FROM cinema
+WHERE MOD(id, 2) <> 0 
+AND description <> 'boring'
+ORDER BY rating DESC;
