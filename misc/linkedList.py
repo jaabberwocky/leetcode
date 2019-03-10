754320b5754d62cbf26c2bc9f34609dd52fb9a50
tobias <tobiasleongzhunmun@gmail.com>
2019-03-10 23:32:12 +0800

    add py implementation of singly-linked list

---
+++ misc/linkedList.py
@@ -0,0 +1,39 @@
+class Node:
+    next = None
+    data = None
+    
+    def __init__(self, data):
+        self.data = data
+
+class LinkedList:
+    head = None
+
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def addHead(self, data):
+        old = self.head
+        new = Node(data)
+        new.next = old
+        self.head = new
+    
+    def __repr__(self):
+        point = self.head
+        while point.next != None:
+            print("Node: %s" % str(point.data))
+            point = point.next
+
+    def __str__(self):
+        point = self.head
+        returnedString = "**Head**\n"
+        while point is not None:
+            returnedString += "Node: %s\n" % str(point.data)
+            point = point.next
+        return returnedString + "**Tail**"
+        
+
+if __name__ == "__main__":
+    l = LinkedList(45)
+    l.addHead(50)
+    l.addHead(30)
+    print(l)
