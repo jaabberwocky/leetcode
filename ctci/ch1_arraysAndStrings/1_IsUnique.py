572972b64cf2ff61a35b3353a849d7d5349b989a
tobias <tobiasleongzhunmun@gmail.com>
2019-03-11 23:10:29 +0800

    add ctci; add ctci_1.1

---
+++ ctci/ch1_arraysAndStrings/1_IsUnique.py
@@ -0,0 +1,35 @@
+import unittest
+
+class Solution:
+    @staticmethod
+    def isUnique(s):
+        '''
+        Returns true if string contains all unique characters.
+        '''
+        chars = {}
+        for ch in s.lower():
+            if ch not in chars:
+                chars[ch] = 1
+            else:
+                return False
+        return True
+
+class Test(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(Solution.isUnique('abcd'), True)
+    
+    def test_2(self):
+        self.assertEqual(Solution.isUnique('aaa'), False)
+
+    def test_3(self):
+        self.assertEqual(Solution.isUnique('aKvC%4'), True)
+
+    def test_4(self):
+        self.assertEqual(Solution.isUnique('aKvC%4a'), False)
+    
+    def test_5(self):
+        self.assertEqual(Solution.isUnique('4561 '), True)
+
+if __name__ == "__main__":
+    unittest.main()
