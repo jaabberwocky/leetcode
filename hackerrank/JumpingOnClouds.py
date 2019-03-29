93f748a41c6c191f74f4ca64d45c208caf35d5a6
tobias <tobiasleongzhunmun@gmail.com>
2019-03-29 23:34:43 +0800

    add jumpingoncloud.pys

---
+++ hackerrank/JumpingOnClouds.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    jumps = 0
+    i = 0
+    while i < len(c):
+        if i == len(c) - 1:
+            # last item
+            break
+        if i < len(c) - 2:
+            if c[i+2] == 0:
+                i += 2
+            else:
+                i += 1
+        else:
+            i += 1
+        jumps += 1
+
+    return jumps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
