6fd6d6dcb4826b7064315381e0cbbc8909f1604a
tobias <leongtobias@gmail.com>
2021-04-28 16:40:39 +0800

    Add MessiGoals.sql

---
+++ codewars/MessiGoals.sql
@@ -0,0 +1,5 @@
+-- https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/sql
+
+-- you will be given a table, goals, with columns la_liga_goals, copa_del_rey_goals, and champions_league_goals. Return a table with a column, res.
+
+select (la_liga_goals + copa_del_rey_goals + champions_league_goals) as res from goals
