a65131f28f8a160f899606114411133933f2893f
tobias <leongtobias@gmail.com>
2021-05-16 21:49:32 +0800

    Add supporting ADTs for Graph

--- grokking_coding_interview/data_structures_primer/graphs/MyGraph.py
+++ grokking_coding_interview/data_structures_primer/graphs/MyGraph.py
@@ -1,12 +1,10 @@
-from LinkedList import LinkedList
+from MyLinkedList import LinkedList
 
 
 class Graph:
     def __init__(self, vertices):
         # Total number of vertices
         self.vertices = vertices
-        # definining a list which can hold multiple LinkedLists
-        # equal to the number of vertices in the graph
         self.array = []
         # Creating a new Linked List for each vertex/index of the list
         for i in range(vertices):
@@ -14,7 +12,7 @@ class Graph:
 
     # Function to add an edge from source to destination
     def add_edge(self, source, destination):
-        if (source < self.vertices and destination < self.vertices):
+        if source < self.vertices and destination < self.vertices:
             self.array[source].insert_at_head(destination)
 
     def print_graph(self):
@@ -26,3 +24,11 @@ class Graph:
                 print("[", temp.data, end=" ] -> ")
                 temp = temp.next_element
             print("None")
+
+
+if __name__ == "__main__":
+    g = Graph(4)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(2, 3)
+    g.print_graph()
